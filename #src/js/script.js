9f98aaf18135f1a91746ee9b1d9ed1bc60aae186
Rasul <58850279+rasul1991@users.noreply.github.com>
2021-08-21 17:47:16 +0500

    editing frontentd 50%

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,3 +1,8 @@
 jQuery(document).ready(function ($) {
-
+    var scene = document.getElementById('scene');
+    var parallaxInstance = new Parallax(scene);
+    var scene = document.getElementById('about');
+    var parallaxInstance = new Parallax(about);
+    var scene = document.getElementById('prod');
+    var parallaxInstance = new Parallax(prod);
 });
