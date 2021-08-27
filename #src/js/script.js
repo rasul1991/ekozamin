d00796f8c74a7e11f462a3d8feeb710096c3302a
Rasul <58850279+rasul1991@users.noreply.github.com>
2021-08-27 10:31:07 +0500

    finish ekozamin

--- #src/js/script.js
+++ #src/js/script.js
@@ -7,24 +7,31 @@ jQuery(document).ready(function ($) {
     var parallaxInstance = new Parallax(prod);
 
     $(function () {
-
-        //BEGIN
         $(".accordion__title").on("click", function (e) {
-
             e.preventDefault();
             var $this = $(this);
-
             if (!$this.hasClass("accordion-active")) {
                 $(".accordion__content").slideUp(400);
                 $(".accordion__title").removeClass("accordion-active");
                 $('.accordion__arrow').removeClass('accordion__rotate');
             }
-
             $this.toggleClass("accordion-active");
             $this.next().slideToggle();
             $('.accordion__arrow', this).toggleClass('accordion__rotate');
         });
-        //END
-
+    });
+    $(function () {
+        $(".accordion__title_s").on("click", function (e) {
+            e.preventDefault();
+            var $this = $(this);
+            if (!$this.hasClass("accordion-active")) {
+                $(".accordion__content").slideUp(400);
+                $(".accordion__title").removeClass("accordion-active");
+                $('.accordion__arrow').removeClass('accordion__rotate');
+            }
+            $this.toggleClass("accordion-active");
+            $this.next().slideToggle();
+            $('.accordion__arrow', this).toggleClass('accordion__rotate');
+        });
     });
 });
